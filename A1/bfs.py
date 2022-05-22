5b5959ce9a7a160dd96d6c9896271e4259a4fb3e
Allan Soares Silva <allan.nwrk@gmail.com>
2022-05-22 04:52:33 -0300

    (A1): reduz o nível de portunhês do código

--- A1/bfs.py
+++ A1/bfs.py
@@ -8,44 +8,44 @@ class VerticeInfo(Vertice):
     
     def __init__(self, v: Vertice):
         self.ref = v
-        self.visited = False
-        self.distance = 0
-        self.parent: Vertice|None = None
+        self.visitado = False
+        self.distancia = 0
+        self.antecessor: Vertice|None = None
         super().__init__(v.rotulo)
         
     def __str__(self) -> str:
         output = (
             f'Vértice: {self.ref.rotulo}',
-            f'Visitado: {self.visited}',
-            f'Distância: {self.distance}',
-            f'Antecessor: {self.parent}'
+            f'Visitado: {self.visitado}',
+            f'Distância: {self.distancia}',
+            f'Antecessor: {self.antecessor}'
         )
         return '\n'.join(output)
         
 
 def bfs(g: Grafo, s: Vertice):
     vertices_infos = list([VerticeInfo(v) for v in g.get_vertices()])
-    queue: List[VerticeInfo] = list()
-    queue.append(s)
+    fila: List[VerticeInfo] = list()
+    fila.append(s)
     
-    level = 0
-    while len(queue) > 0:
-        print(f'{level}:', ','.join([v.rotulo for v in queue]))
-        u = queue.pop(0)
+    nivel = 0
+    while len(fila) > 0:
+        print(f'{nivel}:', ','.join([v.rotulo for v in fila]))
+        u = fila.pop(0)
         for v in g.vizinhos(u):
             v_info = vertices_infos[vertices_infos.index(v)]
-            if (not v_info.visited):
-                v_info.visited = True
-                v_info.distance += 1
-                v_info.parent = u
-                queue.append(v)
-        level += 1
+            if (not v_info.visitado):
+                v_info.visitado = True
+                v_info.distancia += 1
+                v_info.antecessor = u
+                fila.append(v)
+        nivel += 1
 
 
 if __name__ == '__main__':
     g = Grafo.ler('./tests/polbooks.net')
 
-    start_time = time()
+    inicio = time()
     bfs(g, g.get_vertices()[0])
 
-    print(f'Duration: {round(time() - start_time, 3)}s')
+    print(f'Duração: {round(time() - inicio, 3)}s')

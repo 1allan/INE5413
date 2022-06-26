3f5e8cdc92a626a6e7db15f59cff51f9e79cca35
Allan Soares Silva <allan.nwrk@gmail.com>
2022-06-26 15:01:38 -0300

    (A2): finaliza o algoritmo de Kruskal

--- A2/kruskal.py
+++ A2/kruskal.py
@@ -1,23 +1,30 @@
-from typing import List
+from typing import List, Set
 from lib import Grafo, Vertice, Aresta
 
 
-def kruskal(g: Grafo):
-    a: List[Aresta] = []
-    s = { v.rotulo: [] for v in g.get_vertices() }
+def kruskal(g: Grafo) -> Set[Aresta]:
+    agm_arestas: Set[Aresta] = set()
+    arvores = { v.rotulo: { v.rotulo } for v in g.get_vertices() }
     arestas = sorted(g.get_arestas(), key=lambda e: e.peso)
     
-    for aresta in arestas:
-        if s[aresta.u.rotulo] != s[aresta.v.rotulo]:
-            a.append(aresta)
-            x = s[aresta.u.rotulo] + s[aresta.v.rotulo]
+    for (aresta) in arestas:
+        u = aresta.u
+        v = aresta.v
+        if arvores[u.rotulo] != arvores[v.rotulo]:
+            agm_arestas.add(aresta)
+            x = arvores[u.rotulo].union(arvores[v.rotulo])
             for y in x:
-                s[y.rotulo] += x
-                
-    return a
+                arvores[y] = x
+    return agm_arestas
 
 
 if __name__ == '__main__':
-    g = Grafo.ler('./tests/agm_tiny.net')
-    result = kruskal(g)
-    print(result)
+    import os
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, './tests/teste_cfc.net')
+    
+    g = Grafo.ler(filename)
+    agm = kruskal(g)
+    
+    print(sum([ a.peso for a in agm]))
+    print(', '.join([f'{r.u.rotulo}-{r.v.rotulo}' for r in agm]))
